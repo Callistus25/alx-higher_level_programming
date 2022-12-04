b000a1fc84a0511f20c4eb01ef511ce2d1fa4a9b
Callistus25 <callistus.obidiozor@gmail.com>
2022-12-04 14:49:19 -0800

    Task update

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 101):
+        string = ""
+        if num % 3 == 0:
+            string = string + "Fizz"
+        if num % 5 == 0:
+            string = string + "Buzz"
+        if num % 5 != 0 and num % 3 != 0:
+            string = string + str(num)
+        print(string, end=' ')
