7b685ca306b661040417b9cf571eab9889aa4723
Callistus25 <callistus.obidiozor@gmail.com>
2023-01-11 22:34:34 -0800

    code modify

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,38 +1,13 @@
 #!/usr/bin/python3
+"""Class BaseGeometry"""
 
-
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
-
+    """ Class child of BaseGeometry """
     def __init__(self, width, height):
-        """__init__ method
-        Args:
-            width (int): width integer must be greater than 0
-            height (int): height integer must be greater than 0
-        """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-
-if __name__ == '__main__':
-    r = Rectangle(3, 5)
-
-    print(r)
-    print(dir(r))
-
-    try:
-        print("Rectangle: {} - {}".format(r.width, r.height))
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r2 = Rectangle(4, True)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    print(issubclass(Rectangle, BaseGeometry))
+            self.__width = width
+            self.__height = height
+            self.integer_validator("width", self.__width)
+            self.integer_validator("height", self.__height)
